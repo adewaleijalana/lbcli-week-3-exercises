643c14c49011ed7db0d620915a3430d75e3a23a7
Adewale Ijalana <adewaleijalana@adewaleijalana.local>
2025-03-31 07:24:53 +0100

    wallet and getting publick key from address

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
+bitcoin-cli -regtest -rpcwallet=btrustwallet -named getaddressinfo address=$address | jq -r '.pubkey'
